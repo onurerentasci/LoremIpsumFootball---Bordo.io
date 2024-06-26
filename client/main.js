0d8708318a8e16abb96321b849599830d43c6787
Onur Eren Taşcı <82609661+onurerentasci@users.noreply.github.com>
2024-06-26 09:28:23 +0300

    changes main styles, added new imports

--- client/main.js
+++ client/main.js
@@ -1,3 +1,11 @@
 import "./main.html";
 import "bootstrap";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "../client/web/components/leagueTitle/index.html";
+import "../client/web/components/leagueTitle/index.js";
+import "../client/web/components/leagueTitle/styles.css";
+
+import "../client/web/components/leagueSelector/index.html";
+import "../client/web/components/leagueSelector/index.js";
